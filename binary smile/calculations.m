45cc037e5a1f272c13704150caf18bd451ff1636
Hao Sun <psyhs2@nottingham.ac.uk>
2018-11-07 18:22:05 +0000

    Coding the choose attribute in brute force with 3 million iterations

---
+++ binary smile/calculations.m
@@ -0,0 +1,22 @@
+function [positive, negative] = Calculate_Ratio(targets)
+	n = 0;
+	p = 0;
+	for i=1:targets.length
+		if targets(i) == 0
+			n++;
+		else
+			p++;
+		end
+	end
+	return ([positive, negative] = [p, n])
+	% return [positive, negative];
+end
+
+
+function entropy = Calculate_Entropy(p, n)
+	posProb = p / (p+n);
+	negProb = n / (p+n);
+
+	return (entropy = - posProb*log2(posProb) - negProb*log2(negProb))
+end
+
--- binary smile/decisionTree.m
+++ binary smile/decisionTree.m
@@ -5,49 +5,118 @@ inputs = reshape(points,[150, 66*2]);
 % inputs = reshape(points,[66*2,150]);
 targets = labels';
 
+choose_attribute(inputs, targets)
 
-tree = fitctree(inputs,targets);
+% tree = fitctree(inputs,targets);
 
 
 % DrawDecisionTree(tree, "myTree")
 
 
 
-function [best_feature, best_threshold] = choose_attribute(features, targets):
+function [best_feature, best_threshold] = choose_attribute(features, targets)
 
 	% TODO: compare sides to make sure the inputs match
 	
-	[examples, attributes] = size(features)
+	[sampleSize, attributes] = size(features)
 
 
-	if (examples =! len(targets)):
-		disp('Size of inputs and targets does not match');
-		return
+	% if (sampleSize != targets.length):
+		% disp('Size of inputs and targets does not match');
+		% return
 
 
-	% TODO: Calculate the number of positive and neegativ examples
-	n = 0;
-	p = 0;
-	for i=1:examples
-		if targets(i) == 0
-			n++;
-		else
-			p++;
-		end
-	end
-
+	% TODO: Calculate the number of positive and neegativ sampleSize
+	
+	[p, n] = Calculate_Ratio(targets)
+	threshold = 0;
+	bestAttribute = 0;
+	bestThreshold = 0;
+	bestGain = 0;
 
  	for i=1:attributes
 		% TODO: calculate the estimate on informatton contaied
-		estimate = Calculate_Estimate(p,n)
+		entropy = Calculate_Entropy(p,n)
+		for j=1:sampleSize
+			threshold = features(j,i)
+			leftChild = [];
+			rightChild = [];
+			% leftChildPos = 0;
+			% leftChildNeg = 0;
+			% rightChildPos = 0;
+			% rightChildNeg = 0;
+
+			for x=1:sampleSize
+				if features(x,i) > threshold
+					leftChild = [leftChild, x];
+					% leftChildNeg++;
+					% leftChildPos++;
+				else
+					rightChild = [rightChild, x];
+					% rightChildNeg++;
+					% rightChildPos++;
+				end
+			end
+
+			[lp, ln] = Calculate_Ratio(getTargets(leftChild,targets))
+			[rp, rn] = Calculate_Ratio(getTargets(rightChild,targets))
+			% remainder = (lp+ln)/(p+n)*Calculate_Entropy(lp, ln) + (rp+rn)/(p+n)*Calculate_Entropy(rp, rn)
+			remainder = Calculate_Remainder(lp,ln,rp,rn)
+			gain = entropy - remainder;
+			if gain > bestGain
+				bestGain = gain;
+				bestAttribute = i;
+				bestThreshold = threshold;
+			end
+		end
 	end
+
+	best_threshold = bestThreshold
+	bestGain
+	best_feature = bestAttribute
+	% return (best_feature, best_threshold)
 end
 
-function [positive, negative] = 
 
-function estimate = Calculate_Estimate(p, n)
+function t = getTargets(indexes, targets)
+	t = [];
+	for i=1:length(indexes)
+		t = [t, targets(indexes(i))]
+	end
+	% return t;
+end	
+
+
+function [positive, negative] = Calculate_Ratio(targets)
+	n = 0;
+	p = 0;
+	for i=1:length(targets)
+		if targets(i) == 0
+			n=n+1;
+		else
+			p=p+1;
+		end
+	end
+	% [positive, negative] = p, n;
+	positive = p;
+	negative = n;
+	% return [positive, negative];
+end
+
+
+function entropy = Calculate_Entropy(p, n)
 	posProb = p / (p+n);
 	negProb = n / (p+n);
 
-	estimate = - posProb*log2(posProb) - negProb*log2(negProb)
+	entropy = - posProb*log2(posProb) - negProb*log2(negProb)
 end
+
+
+function remainder = Calculate_Remainder(lp, ln, rp, rn)
+	total = lp+ln+rp+rn;
+	remainder = (lp+ln)/total*Calculate_Entropy(lp, ln) + (rp+rn)/total*Calculate_Entropy(rp, rn)
+end
+
+
+% function information_Gain
+
