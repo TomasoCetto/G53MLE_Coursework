acd6a36623449655ed8978ec00ca3f6a2d576a97
Hao Sun <psyhs2@nottingham.ac.uk>
2018-11-10 14:59:17 +0000

    finish cross validation

--- binary smile/crossvalidation.m
+++ binary smile/crossvalidation.m
@@ -4,21 +4,27 @@ load('labels.mat');
 inputs = reshape(points,[66*2, 150])';
 targets = labels';
 
-function cv = crossvalidation(inputs,targets)
+[p, r, f] = crossvalidations(inputs, targets);
 
+function [precisions, recalls, f1s] = crossvalidations(inputs,targets)
+
+    k = 10;
     c = cvpartition(length(inputs),'KFold', k);
 
     trees = cell(1,10)
+    recalls = zeros(1,k);
+    precisions = zeros(1,k);
+    f1s = zeros(1,k);
 
     for i=1:c.NumTestSets
         trIDX = training(c,i);
         teIDX = test(c,i);
 
-        trainingInputs = inputs(:,trIDX);
-        trainingTargets = targets(:,trIDX);
+        trainingInputs = inputs(trIDX,:);
+        trainingTargets = targets(trIDX);
 
-        testingInputs = inputs(:,teIDX);
-        testingTargets = targets(:,teIDX);
+        testingInputs = inputs(teIDX,:);
+        testingTargets = targets(teIDX);
 
 
         %Create 10 trained trees
@@ -31,9 +37,16 @@ function cv = crossvalidation(inputs,targets)
             outputArray = [outputArray,value];
         end
 
+        confusion = confusion_matrix(outputArray, testingTargets);
 
+        recalls(i) = confusion(1,1)/(confusion(1,1)+confusion(1,2));
+        precisions(i) = confusion(1,1)/(confusion(1,1)+confusion(2,1));
+        f1s(i) = 2*((precisions(i)*recalls(i))/(precisions(i)+recalls(i)));
         %recall and precision rates 
     end
+    recalls
+    precisions
+    f1s
 end
 
 
@@ -42,11 +55,28 @@ function output = evaluateOneSample(tree, input)
     if tree.kids == []
         output = tree.class
         return
-    else if input(:,tree.attribute) > tree.threshold
+    elseif input(:,tree.attribute) > tree.threshold
             evaluateOneSample(tree.kids{1},input)
     else
             evaluateOneSample(tree.kids{2},input)
-        end
-     
+        % end
+    end
 end
     
+
+function cm = confusion_matrix(outputs, labels)
+    % cm = zeros(2);
+    tp=0;tn=0;fp=0;fn=0;
+    for i=1:length(outputs)
+        if (labels(i) == 1) && (outputs(i)==1)
+            tp=tp+1;
+        elseif (labels(i) == 0) && (outputs(i)==0)
+            tn=tn+1;
+        elseif (labels(i) == 1) && (outputs(i)==0)
+            fn=fn+1;
+        else
+            fp=fp+1
+        end
+    end
+    cm = [tp, fn; fp, tn];
+end
--- binary smile/decisionTree.m
+++ binary smile/decisionTree.m
@@ -5,13 +5,92 @@ inputs = reshape(points,[66*2, 150])';
 % inputs = reshape(points,[66*2,150]);
 targets = labels';
 
-tree = decisionTreeLearning(inputs, targets);
-DrawDecisionTree(tree, "myTree")
+% tree = decisionTreeLearning(inputs, targets);
+% DrawDecisionTree(tree, "myTree")
 
 % tree = fitctree(inputs,targets);
 % DrawDecisionTree(tree, "myTree")
 
+% [p, r, f] = 
+crossvalidations(inputs, targets);
 
+function crossvalidations(inputs,targets)
+
+    k = 10;
+    c = cvpartition(length(inputs),'KFold', k);
+
+    trees = cell(1,10)
+    recalls = zeros(1,k);
+    precisions = zeros(1,k);
+    f1s = zeros(1,k);
+
+    for i=1:c.NumTestSets
+        trIDX = training(c,i);
+        teIDX = test(c,i);
+
+        trainingInputs = inputs(trIDX,:);
+        trainingTargets = targets(trIDX);
+
+        testingInputs = inputs(teIDX,:);
+        testingTargets = targets(teIDX);
+
+
+        %Create 10 trained trees
+        trees{i} = decisionTreeLearning(trainingInputs,trainingTargets);
+        DrawDecisionTree(trees{i}, "myTree")
+        %tree = trees{i}
+        %use the trained tree to classify my data 
+        outputArray = zeros(size(testingInputs,1));
+        disp(length(testingInputs));
+        for j=1:size(testingInputs, 1)
+            value = evaluateOneSample(trees{i},testingInputs(j,:));
+            outputArray(j) = value;
+        end
+
+        confusion = confusion_matrix(outputArray, testingTargets);
+
+        recalls(i) = confusion(1,1)/(confusion(1,1)+confusion(1,2));
+        precisions(i) = confusion(1,1)/(confusion(1,1)+confusion(2,1));
+        f1s(i) = 2*((precisions(i)*recalls(i))/(precisions(i)+recalls(i)));
+        %recall and precision rates 
+    end
+    recalls
+    precisions
+    f1s
+end
+
+
+
+function output = evaluateOneSample(tree, input)
+    if isempty(tree.kids)
+        output = tree.class;
+        return
+    elseif input(tree.attribute) > tree.threshold
+            output = evaluateOneSample(tree.kids{1},input);
+    else
+            output = evaluateOneSample(tree.kids{2},input);
+        % end
+    end
+end
+    
+
+function cm = confusion_matrix(outputs, labels)
+    % cm = zeros(2);
+    tp=0;tn=0;fp=0;fn=0;
+    length(outputs)
+    for i=1:length(outputs)
+        if (labels(i) == 1) && (outputs(i)==1)
+            tp=tp+1;
+        elseif (labels(i) == 0) && (outputs(i)==0)
+            tn=tn+1;
+        elseif (labels(i) == 1) && (outputs(i)==0)
+            fn=fn+1;
+        else
+            fp=fp+1;
+        end
+    end
+    cm = [tp, fn; fp, tn];
+end
 
 
 function tree = decisionTreeLearning(inputs, targets)
@@ -71,9 +150,9 @@ function [best_feature, best_threshold] = choose_attribute(features, targets)
 	[sampleSize, attributeSize] = size(features);
 
 
-	% if (sampleSize != targets.length):
-		% disp('Size of inputs and targets does not match');
-		% return
+	% if ~(sampleSize == targets.length):
+	% 	disp('Size of inputs and targets does not match');
+	% 	return
 
 
 	% TODO: Calculate the number of positive and neegativ sampleSize
@@ -124,7 +203,7 @@ function [best_feature, best_threshold] = choose_attribute(features, targets)
 
 	best_threshold = bestThreshold;
 % 	bestGain
-    fprintf("bestGain = %f\n", bestGain);
+    % fprintf("bestGain = %f\n", bestGain);
 	best_feature = bestAttribute;
 	% return (best_feature, best_threshold)
 end    
